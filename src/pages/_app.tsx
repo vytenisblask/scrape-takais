d9aec2874e8cab1f941a195a7730fd1ae0d9add4
Vytenis <vytenis@Vyteniss-MacBook-Air.local>
2023-09-07 16:56:29 +0300

    Title and favicon changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,19 @@ import { ThemeProvider } from 'styled-components';
 import type { AppProps } from 'next/app';
 import GlobalStyles from '../styles/GlobalStyles';
 import theme from '../styles/theme';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
-}
+    return (
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Scrape-a-roonie</title>
+        </Head>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    );
+  }
+  
 
 export default MyApp;
