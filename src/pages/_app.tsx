025dbe8a46df133bd9451c55cf44dc6bbcbc8ff6
vytenisblask <vytenisb@yahoo.com>
2024-01-18 21:45:21 +0200

    bug fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import theme from "./theme";
-import NavigationBar from "../pages/components/NavigationBar";
+import theme from "./styles/theme";
+import NavigationBar from "./components/NavigationBar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
