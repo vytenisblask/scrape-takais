8e6dcf3464ba32a0cecd28094017d7ca742900aa
vytenisblask <vytenisb@yahoo.com>
2024-01-18 22:00:10 +0200

    structure update again

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import theme from "./styles/theme";
+import theme from "../styles/theme";
 import NavigationBar from "./components/NavigationBar";
 
 function MyApp({ Component, pageProps }: AppProps) {
