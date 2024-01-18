e2d970993fe49c012c5f5f8525ff9fc8d2422cd6
vytenisblask <vytenisb@yahoo.com>
2024-01-18 21:01:15 +0200

    chakra UI updates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,17 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { ThemeProvider } from "styled-components";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import theme from "../styles/theme";
-import NavigationBar from "../pages/components/NavigationBar"; // Import the component
+import theme from "./theme";
+import NavigationBar from "../pages/components/NavigationBar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <ThemeProvider theme={theme}>
-        <Head>
-          <title>Scrape-a-roonie</title>
-        </Head>
-        <NavigationBar /> {/* Include the NavigationBar */}
-        <Component {...pageProps} />
-      </ThemeProvider>
+    <ChakraProvider theme={theme}>
+      <Head>
+        <title>Scrape-a-roonie</title>
+      </Head>
+      <NavigationBar />
+      <Component {...pageProps} />
     </ChakraProvider>
   );
 }
---
+++ src/pages/theme.ts
@@ -0,0 +1,12 @@
+// theme.ts
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: true,
+  },
+  // ... other theme configurations
+});
+
+export default theme;
